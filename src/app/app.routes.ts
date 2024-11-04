f6df44b99056c52d3ce1816d1b99c7ace0abdffa
AGh123 <ahmad2000gh@gmail.com>
2024-11-04 16:46:39 +0200

    Implemented routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CatalogComponent } from './catalog/catalog.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent, title: 'Home' },
+  { path: 'catalog', component: CatalogComponent, title: 'Catalog' },
+  { path: 'about', component: AboutComponent, title: 'About' },
+];
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { DrawerService } from '../shared/services/drawer.service';
 import { DrawerComponent } from '../shared/drawer/drawer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [MatIconModule, DrawerComponent],
+  imports: [MatIconModule, DrawerComponent, RouterLink],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
